acce5ba73fdf2f5b337fda502ced24d97172e492
Js-Saito <155764273+Js-Saito@users.noreply.github.com>
2024-09-11 00:29:09 +0000

    Ajustando para deploy na vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <div><h1>Bianca eres mui guapa</h1><img src="https://media1.tenor.com/m/vb623iaCi6kAAAAC/snoopy-love.gif"/></div>
+    return <div><h1>Bianca meu leãozinho, eu te amo. Se me ama também dá uma risadinha</h1><img src="https://media1.tenor.com/m/1g_nsiVhqy0AAAAC/snoopy-hello.gif"/></div>
 }
 
 export default Home;
