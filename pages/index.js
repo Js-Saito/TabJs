d4d8985a6662b4301f2b156ebab9169feefffc44
Js-Saito <155764273+Js-Saito@users.noreply.github.com>
2025-08-21 23:41:05 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <div><h1>Bianca meu leãozinho, eu te amo. Se me ama também dá uma risadinha</h1><img src="https://media1.tenor.com/m/1g_nsiVhqy0AAAAC/snoopy-hello.gif"/></div>
+  return (
+    <div>
+      <h1>
+        Bianca meu leãozinho, eu te amo. Se me ama também dá uma risadinha
+      </h1>
+      <img src="https://media1.tenor.com/m/1g_nsiVhqy0AAAAC/snoopy-hello.gif" />
+    </div>
+  );
 }
-
+function teste1() {
+  console.log("indentação errada");
+}
 export default Home;
